66739fec72d5a7806a9816f3304e3238872b1274
Maamoun Haj Najeeb <87011464+maamoun3911@users.noreply.github.com>
2022-09-22 06:55:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-import requests
-import time
+
+import requests, time
 
 APIKey = '74ba0d3e-ab8e-46ee-9285-5c5ca9342e76'
 BotAPIToken = '5518808804:AAGGqq-Oh8LA0acf1st6AyI_oHCMagK65_U'
 chat_id = '772715406'
-limit = 59000
-time_intervel = 5
+limit, time_intervel = 59000, 5*60
 
 def get_price():
     url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-    
+
     parameters = {
         'start':'1',
         'limit':'1',
@@ -23,21 +22,15 @@ def get_price():
     response = requests.get(url, headers=headers, params=parameters).json()
     Currency_price = response['data'][1]['quote']['USD']['price']
     return Currency_price
-# print(get_price())
 
 def send_update(chat_id, msg):
     url = f"https://api.telegram.org/bot{BotAPIToken}/sendMessage?chat_id={chat_id}&text={msg}"
     requests.get(url)
 
-    # chat_id, text
-
 def main_fun():
     while True:
-    # i = 0
-    # while i < 1:
         price = get_price()
         if price < limit:
             send_update(chat_id, f"بقولك ايه يا مأمون سعر البيتكوين{price}")
-        # i += 1
         time.sleep(time_intervel)
 main_fun()
